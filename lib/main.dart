5bac1b13a15b3232981ecece5d3680449d083449
Vinicius Gomes Correia <viniciusgomesccc10@gmail.com>
2023-08-27 23:00:00 -0300

    conexão com o firebase

--- lib/main.dart
+++ lib/main.dart
@@ -16,8 +16,14 @@ import 'package:iboss/repositories/business/wage_repository.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MultiProvider(providers: [
     ChangeNotifierProvider(
         create: (context) => CashPaymentRepository(cashPayments: [])),
